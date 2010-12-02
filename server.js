a559477b9fb071336f3f6c61d587b57fea079eb9
Kevin Gorski <kevin.gorski@gmail.com>
2010-12-02 15:30:48 -0700

    Displaying a repository information form

--- server.js
+++ server.js
@@ -2,7 +2,15 @@ var http = require('http');
 
 http.createServer(function (request, response) {
 	response.writeHead(200, {'Content-Type': 'text/html'});
-	response.end('<html><body>Hello World</body></html>');
+	response.end('<html><body>'
+		+ '<h1>GitHub Repository Commit Monitor</h1>'
+		+ '<form method="post" action="."><fieldset>'
+		+ '<div><label for="UserName">User Name:</label><input type="text" id="UserName" name="UserName" /></div>'
+		+ '<div><label for="Repository">Repository:</label><input type="text" id="Repository" name="Repository" /></div>'
+		+ '<div><label for="Branch">Branch:</label><input type="text" id="Branch" name="Branch" value="master" /></div>'
+		+ '<div><input id="ListCommits" type="submit" value="List Commits" /></div>'
+		+ '</fieldset></form>'
+		+ '</body></html>');
 }).listen(8124);
 
 console.log('Server running at http://127.0.0.1:8124/');
