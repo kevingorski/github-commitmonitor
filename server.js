eb4a6c24fb5995ef81cb7eb87ddc46d1d73e1fc7
Kevin Gorski <kevin.gorski@gmail.com>
2010-12-02 15:15:43 -0700

    Returning HTML instead of plaintext

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var http = require('http');
 
 http.createServer(function (request, response) {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('Hello World\n');
+	response.writeHead(200, {'Content-Type': 'text/html'});
+	response.end('<html><body>Hello World</body></html>');
 }).listen(8124);
 
 console.log('Server running at http://127.0.0.1:8124/');
