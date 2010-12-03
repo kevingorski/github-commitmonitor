7ea51297442a4d8690761a24663fadb5ea0d3f78
Kevin Gorski <kevin.gorski@gmail.com>
2010-12-03 10:51:34 -0700

    Listing commits or displaying a error message

--- server.js
+++ server.js
@@ -13,10 +13,65 @@ http.createServer(function (request, response) {
 		
 		request.on('end', function() {
 			var posted = querystring.parse(requestBody);
+			var github = http.createClient(80, 'github.com');
+			var request = github.request('GET', 
+				'/api/v2/json/commits/list/'
+					+ posted.UserName + '/'
+					+ posted.Repository + '/'
+					+ posted.Branch,
+				{'host': 'github.com'});
 			
-			response.writeHead(200, {'Content-Type': 'text/html'});
+			request.on('response', function(ghr) {
+				ghr.setEncoding('utf8');
+				
+				response.writeHead(200, {'Content-Type': 'text/html'});
+				response.write('<html><head><title>GitHub Repository Commit Monitor</title></head><body>'
+					+ '<h1>GitHub Repository Commit Monitor</h1>'
+					+ '<form method="post" action="." enctype="application/x-www-form-urlencoded"><fieldset>'
+					+ '<div><label for="UserName">User Name:</label><input type="text" id="UserName" name="UserName" '
+					+ 'value="' + posted.UserName + '" /></div>'
+					+ '<div><label for="Repository">Repository:</label><input type="text" id="Repository" name="Repository" '
+					+ 'value="' + posted.Repository + '" /></div>'
+					+ '<div><label for="Branch">Branch:</label><input type="text" id="Branch" name="Branch" '
+					+ 'value="' + posted.Branch + '"/></div>'
+					+ '<div><input id="ListCommits" type="submit" value="List Commits" /></div>'
+					+ '</fieldset></form>');
+				
+				if(ghr.statusCode == 200) {
+					var githubData = '';
+					
+					ghr.on('data', function(chunk) {
+						githubData += chunk;
+					});
+					
+					ghr.on('end', function() {
+						var data = JSON.parse(githubData);
+					
+						response.write('<a href="http://github.com/' 
+							+ posted.UserName + '/' 
+							+ posted.Repository + '" >' 
+							+ posted.UserName + '/' + posted.Repository + '</a>');
+					
+						response.write('<ul>');
+						
+						for (var i = 0; i < data.commits.length; i++) {
+							var commit = data.commits[i];
+	
+							response.write('<li><a href="http://github.com' + commit.url + '">' 
+								+ commit.committed_date + '</a> - ' + commit.author.name
+								+ ': ' + commit.message + '</li>');
+						}
+						
+						response.end('</ul></body></html>');
+					});
+				} else {
+					response.write('<h2>Failed</h2>');
+					
+					response.end(utils.inspect(posted));
+				}
+			});
 			
-			response.end(utils.inspect(posted));
+			request.end();
 		});
 		
 	} else {
