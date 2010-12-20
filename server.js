339d26db6f5177fa34d6bb8d2819f52e63ce4a5b
Kevin Gorski <kevin.gorski@gmail.com>
2010-12-19 17:06:38 -0700

    Moving markup to Jade views

--- server.js
+++ server.js
@@ -1,25 +1,9 @@
 var http = require('http');
 var express = require('express');
 
-var displayHistory = function(request, response) {
-	if(request.session.history.length) {
-		response.write('<h3>Recently Viewed</h3><ol>');
-		
-		for(var i = 0; i < request.session.history.length; i++) {
-			var item = request.session.history[i];
-			
-			response.write('<li><a href="http://github.com/' 
-				+ item.UserName + '/' 
-				+ item.Repository + '" >' 
-				+ item.UserName + '/' + item.Repository + '</a></li>');
-		}
-		
-		response.write('</ol>');
-	}
-};
-
 var handlePost = function(request, response) {
 	var posted = request.body;
+	var history = request.session.history;
 	var github = http.createClient(80, 'github.com');
 	var ghRequest = github.request('GET', 
 		'/api/v2/json/commits/list/'
@@ -29,24 +13,8 @@ var handlePost = function(request, response) {
 		{'host': 'github.com'});
 	
 	ghRequest.on('response', function(ghr) {
-		response.writeHead(200, {'Content-Type': 'text/html'});
-		response.write('<html><head><title>GitHub Repository Commit Monitor</title><link rel="stylesheet" href="style.css" ></head><body>'
-			+ '<h1>GitHub Repository Commit Monitor</h1>'
-			+ '<form method="post" action="." enctype="application/x-www-form-urlencoded"><fieldset>'
-			+ '<div><label for="UserName">User Name:</label><input type="text" id="UserName" name="UserName" '
-			+ 'value="' + posted.UserName + '" /></div>'
-			+ '<div><label for="Repository">Repository:</label><input type="text" id="Repository" name="Repository" '
-			+ 'value="' + posted.Repository + '" /></div>'
-			+ '<div><label for="Branch">Branch:</label><input type="text" id="Branch" name="Branch" '
-			+ 'value="' + posted.Branch + '"/></div>'
-			+ '<input id="ListCommits" type="submit" value="List Commits" />'
-			+ '</fieldset></form>');
-		
-		displayHistory(request, response);
-		
 		if(ghr.statusCode == 200) {
 			var githubData = '';
-			var history = request.session.history;
 			
 			history.unshift(posted);
 			
@@ -59,26 +27,11 @@ var handlePost = function(request, response) {
 			
 			ghr.on('end', function() {
 				var data = JSON.parse(githubData);
-			
-				response.write('<a href="http://github.com/' 
-					+ posted.UserName + '/' 
-					+ posted.Repository + '" >' 
-					+ posted.UserName + '/' + posted.Repository + '</a>');
-			
-				response.write('<ul>');
 				
-				for (var i = 0; i < data.commits.length; i++) {
-					var commit = data.commits[i];
-
-					response.write('<li><a href="http://github.com' + commit.url + '">' 
-						+ commit.committed_date + '</a> - ' + commit.author.name
-						+ ': ' + commit.message + '</li>');
-				}
-				
-				response.end('</ul></body></html>');
+				response.render('index', { locals: { history: history, posted: posted, commits: data.commits }});
 			});
 		} else {
-			response.end('<h2>That user, repository, or branch doesn\'t seem to exist.</h2></body></html>');
+			response.render('index', { locals: { history: history, posted: posted, commits: [] }});
 		}
 	});
 	
@@ -89,34 +42,21 @@ var handleGet = function(request, response) {
 	if(!request.session.history)
 		request.session.history = [];
 	
-	// Write out the empty form
-	response.writeHead(200, {'Content-Type': 'text/html'});
-	response.write('<html><head>'
-		+ '<title>GitHub Repository Commit Monitor</title>'
-		+ '<link rel="stylesheet" href="style.css" ></head><body>'
-		+ '<h1>GitHub Repository Commit Monitor</h1>'
-		+ '<form method="post" action="." enctype="application/x-www-form-urlencoded"><fieldset>'
-		+ '<div><label for="UserName">User Name:</label><input type="text" id="UserName" name="UserName" /></div>'
-		+ '<div><label for="Repository">Repository:</label><input type="text" id="Repository" name="Repository" /></div>'
-		+ '<div><label for="Branch">Branch:</label><input type="text" id="Branch" name="Branch" value="master" /></div>'
-		+ '<input id="ListCommits" type="submit" value="List Commits" />'
-		+ '</fieldset></form>');
-		
-	displayHistory(request, response);
-	
-	response.end('</body></html>');
+	response.render('index', { locals: { posted: { Branch: 'master'}, history: request.session.history, commits: []}});
 };
 
 var server = express.createServer(
 	express.bodyDecoder(),
 	express.cookieDecoder(),
 	express.session(),
-	express.staticProvider({ root: __dirname + '/public', cache: true }),
-	express.router(function(app) {
-		app.get('/', handleGet);
-		app.post('/', handlePost);
-	})
+	express.staticProvider({ root: __dirname + '/public', cache: true })
 );
+
+server.set('views', __dirname + '/views');
+server.set('view engine', 'jade');
+
+server.get('/', handleGet);
+server.post('/', handlePost);
 
 server.listen(8124);
 
