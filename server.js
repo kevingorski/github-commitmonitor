72b84057f6bf380e75d221a95c8cb52af75883f4
Kevin Gorski <kevin.gorski@gmail.com>
2011-03-19 13:07:36 -0600

    Parsing duostack db uri

--- server.js
+++ server.js
@@ -98,13 +98,20 @@ server.configure('development', function() {
 });
 	
 server.configure('production', function() {
+	var parseUrl = require('url').parse,
+		db_uri = parseUrl(process.env['DUOSTACK_DB_MONGODB']);
+	
 	log.level = Log.WARNING;
 	log.stream = fs.createWriteStream('log/GitHubCommitMonitor.log', { flags: 'a' });
 
 	server.use(express.session({
 		secret: 'GHCMNNFTW',
 		store: new MongoStore({
-			db: process.env['DUOSTACK_DB_MONGODB']
+			db: db_uri.pathname.substr(1),
+			host: db_uri.hostname,
+			port: parseInt(db_uri.port),
+			username: db_uri.auth.split(':')[0],
+			password: db_uri.auth.split(':')[1]
 		})
 	}));
 	server.use(assetManager({ 
